115196273845f0babcf59fe1ff3d7fc9b83371d5
MehdiDev76 <elkoukhelmehdi1@gmail.com>
2018-01-05 03:59:47 +0000

    setup of the express js server

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var morgan = require('morgan')
+
+var app = express();
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}your user was registered`
+        })
+    })
+
+
+app.listen(process.env.PORT || 8081)
